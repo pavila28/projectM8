e2949516d49b617837ec14ca1a4fe17a6304208f
Patricio Avila <pavila@vialterna.com>
2023-12-11 10:17:56 -0600

    Product completed

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const Product = require('../model/productModel')
+const { findByIdAndDelete } = require('../model/productModel')
 
 const getProduct = asyncHandler ( async (req, res) => {
     const products = await Product.find()
@@ -23,11 +24,30 @@ const createProduct = asyncHandler ( async (req, res) => {
 })
 
 const updateProduct = asyncHandler ( async (req, res) => {
-    res.status(200).json({ message: `Producto actualizado: ${req.params.id}` })
+    const product = await Product.findById(req.params.id)
+
+    if(!product){
+        res.status(400)
+        throw new Error('El producto no fue encontrado')
+    }
+
+    const productUpdated = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    res.status(200).json(productUpdated)
 })
 
 const deleteProduct = asyncHandler ( async (req, res) => {
-    res.status(200).json({ message: `Producto eliminado: ${req.params.id}` })
+    const product = await Product.findById(req.params.id)
+
+    if(!product){
+        res.status(400)
+        throw new Error('El producto no fue encontrado')
+    }
+
+    await Product.deleteOne(product)
+    //await Product.findByIdAndDelete(req.params.id)
+
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
